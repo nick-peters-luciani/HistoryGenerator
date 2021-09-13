da50fd2e27b69617f1a6090d523735d73397534f
Nick Peters-Luciani <nick.m.luciani@gmail.com>
2021-09-12 23:01:53 -0500

    Add more options to CivilizationRenderer

--- Processes/Renderers/CivilizationRenderer.cs
+++ Processes/Renderers/CivilizationRenderer.cs
@@ -1,4 +1,5 @@
 ﻿using HistoryGenerator.Core.Processing;
+using HistoryGenerator.Core.Settings;
 using HistoryGenerator.Models;
 using HistoryGenerator.Processes.Generators;
 using HistoryGenerator.Processes.Renderers;
@@ -10,18 +11,38 @@ namespace HistoryGenerator.Systems
 	[Process(ProcessChain = "Render", Dependencies = new[] { typeof(CivilizationGenerator), typeof(ClimateMapRenderer) })]
 	public class CivilizationRenderer : RenderProcess
 	{
+		[NumberSetting]
+		public float PinSize { get; set; } = 5;
+
+		[ColorSetting]
+		public Color PinColor { get; set; } = Color.Yellow;
+
+		[ColorSetting]
+		public Color TextColor { get; set; } = Color.White;
+
+		[BooleanSetting]
+		public bool TextShadow { get; set; } = true;
+
 		private static Font Font = new Font(SystemFonts.DefaultFont, FontStyle.Bold);
 
 		public override void Render(ProcessUnit processUnit, Bitmap bitmap, Graphics graphics)
 		{
 			List<Civilization> civilizations = processUnit.Get<List<Civilization>>("Civilizations");
 
+			Brush pinBrush = new SolidBrush(PinColor);
+			Brush textBrush = new SolidBrush(TextColor);
+
 			foreach (Civilization civ in civilizations)
 			{
+				graphics.FillRectangle(pinBrush, civ.Location.X, civ.Location.Y, PinSize, PinSize);
+				
 				float l = civ.Name.Length * 5;
-				graphics.FillRectangle(Brushes.Yellow, civ.Location.X, civ.Location.Y, 4, 4);
-				graphics.DrawString(civ.Name, Font, Brushes.Black, civ.Location.X - l / 2 - 1, civ.Location.Y - 14);
-				graphics.DrawString(civ.Name, Font, Brushes.White, civ.Location.X - l / 2, civ.Location.Y - 15);
+				if (TextShadow)
+				{
+					graphics.DrawString(civ.Name, Font, Brushes.Black, civ.Location.X - l / 2 - 1, civ.Location.Y - 14);
+				}
+				
+				graphics.DrawString(civ.Name, Font, textBrush, civ.Location.X - l / 2, civ.Location.Y - 15);
 			}
 		}
 	}
