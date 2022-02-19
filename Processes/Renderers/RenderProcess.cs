f8393e15afe7e31747bf130e8d81d3e7fbd4cf3d
Nick Peters-Luciani <nick.m.luciani@gmail.com>
2022-02-18 20:54:20 -0600

    Improve speed of render processes

--- Processes/Renderers/RenderProcess.cs
+++ Processes/Renderers/RenderProcess.cs
@@ -2,6 +2,8 @@
 using HistoryGenerator.Core.Settings;
 using System;
 using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
 using static HistoryGenerator.Utility.ColorUtil;
 
 namespace HistoryGenerator.Processes.Renderers
@@ -27,14 +29,27 @@ namespace HistoryGenerator.Processes.Renderers
 
 		protected void ForEachPixel(Bitmap bitmap, Func<int, int, Color> colorFunc)
 		{
+			// Using Bitmap.LockBits is more performant than GetPixel and SetPixel
+			// See: https://docs.microsoft.com/en-us/dotnet/api/system.drawing.bitmap.lockbits
+			Rectangle bounds = new Rectangle(0, 0, bitmap.Width, bitmap.Height);
+			BitmapData bitmapData = bitmap.LockBits(bounds, ImageLockMode.ReadWrite, bitmap.PixelFormat);
+			IntPtr ptr = bitmapData.Scan0;
+        	int[] rgbValues = new int[bitmap.Width * bitmap.Height];
+			Marshal.Copy(ptr, rgbValues, 0, rgbValues.Length);
+
 			for (int x=0; x<bitmap.Width; x++)
 			{
 				for (int y=0; y<bitmap.Height; y++)
 				{
-					Color color = bitmap.GetPixel(x,y).Blend(colorFunc(x,y), BlendMode);
-					bitmap.SetPixel(x, y, color);
+					int index = x + (y * bitmap.Width);
+					Color color = Color.FromArgb(rgbValues[index]);
+					color = color.Blend(colorFunc(x,y), BlendMode);
+					rgbValues[index] = color.ToArgb();
 				}
 			}
+			
+			Marshal.Copy(rgbValues, 0, ptr, rgbValues.Length);
+			bitmap.UnlockBits(bitmapData);
 		}
 	}
 }
